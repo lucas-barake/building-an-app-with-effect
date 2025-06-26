babbebe43ec4d75363298a53653c91ec6fef2ddf
lucas-barake <86905052+lucas-barake@users.noreply.github.com>
2025-06-26 00:15:37 -0500

    sync

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,6 +1,38 @@
-import { NodeRuntime } from "@effect/platform-node";
-import { Console } from "effect";
+import {
+  HttpApi,
+  HttpApiBuilder,
+  HttpApiEndpoint,
+  HttpApiGroup,
+  HttpServer,
+} from "@effect/platform";
+import { NodeHttpServer, NodeRuntime } from "@effect/platform-node";
+import { Effect, Layer, Schema } from "effect";
+import { createServer } from "node:http";
 
-const program = Console.log("server.ts");
+class HealthGroup extends HttpApiGroup.make("health")
+  .add(HttpApiEndpoint.get("get", "/").addSuccess(Schema.String))
+  .prefix("/health") {}
 
-NodeRuntime.runMain(program);
+const Api = HttpApi.make("Api").add(HealthGroup);
+
+const HealthGroupLive = HttpApiBuilder.group(Api, "health", (handlers) =>
+  Effect.gen(function* () {
+    yield* Effect.logDebug("HealthGroupLive");
+
+    return handlers.handle("get", () => Effect.succeed("OK"));
+  }),
+);
+
+const ApiLive = HttpApiBuilder.api(Api).pipe(Layer.provide(HealthGroupLive));
+
+const HttpLive = HttpApiBuilder.serve().pipe(
+  HttpServer.withLogAddress,
+  Layer.provide(ApiLive),
+  Layer.provide(
+    NodeHttpServer.layer(createServer, {
+      port: 3000,
+    }),
+  ),
+);
+
+NodeRuntime.runMain(Layer.launch(HttpLive));
